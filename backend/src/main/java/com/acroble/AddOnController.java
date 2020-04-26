d33ca0e3914c5212e24b512e4b462700df3d48bc
Adlan <adlan@acroble.com>
2020-04-26 18:41:13 +0800

    Add basic project scaffolding with Spring Boot and Vue.js

---
+++ backend/src/main/java/com/acroble/AddOnController.java
@@ -0,0 +1,40 @@
+package com.acroble;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/api")
+public class AddOnController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AddOnController.class);
+
+    public static final String HELLO_TEXT = "Hello from Spring Boot Backend!";
+    public static final String SECURED_TEXT = "Hello from the secured resource!";
+
+
+    @RequestMapping(path = "/hello")
+    public String sayHello() {
+        LOG.info("GET called on /hello resource");
+        return HELLO_TEXT;
+    }
+
+    @RequestMapping(path="/secured", method = RequestMethod.GET)
+    public @ResponseBody String getSecured() {
+        LOG.info("GET successfully called on /secured resource");
+        return SECURED_TEXT;
+    }
+
+    // Forwards all routes to FrontEnd except: '/', '/index.html', '/api', '/api/**'
+    // Required because of 'mode: history' usage in frontend routing, see README for further details
+    @RequestMapping(value = "{_:^(?!index\\.html|api).*$}")
+    public String redirectApi() {
+        LOG.info("URL entered directly into the Browser, so we need to redirect...");
+        return "forward:/";
+    }
+
+}
